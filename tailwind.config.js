3610866dc287357771c8c14d60023e709c7712b3
trihdm-0498 <ho.dang.minh.tri@sun-asterisk.com>
2024-08-30 12:14:03 +0700

    Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./index.html",
-  "./src/**/*.{vue,js,ts,jsx,tsx}",
-];
-export const theme = {
-  extend: {},
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
-export const plugins = [];
